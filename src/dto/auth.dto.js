015b1fa380b208c13f8d1dc423b1dff667bf642d
sebastian rodriguez <juan.sebas-722@hotmail.com>
2023-10-18 21:26:34 -0500

    auth.dto register and login Dto implemented

--- src/dto/auth.dto.js
+++ src/dto/auth.dto.js
@@ -1,2 +1,41 @@
+import { body } from "express-validator";
+
+
+export const registerDto = [
+  body('name', 'This name is wrong')
+    .trim()
+    .isString(),
+
+  body('email', 'This email is wrong')
+    .trim()
+    .isEmail()
+    .normalizeEmail(),
+
+  body('password', 'Incorrect password')
+    .trim()
+    .isLength({ min: 6 }),
+
+  body('repassword', 'Passwords do not match')
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('You did not enter the same password, check both');
+      }
+      return value;
+    }),
+
+  body('roles', 'Incorrect roles').isArray(),
+];
+
+export const loginDto = [
+  body('email', 'This email is wrong')
+    .trim()
+    .isEmail()
+    .normalizeEmail(),
+
+  body('password', 'Incorrect password')
+    .trim()
+    .isLength({ min: 6 }),
+];
+
 
 
