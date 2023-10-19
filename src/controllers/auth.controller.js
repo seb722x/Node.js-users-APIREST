f53b99e85ac807fae86ddb7d62185f2f563b4e3d
sebastian rodriguez <juan.sebas-722@hotmail.com>
2023-10-18 21:22:35 -0500

    auth.controller.js login function added to manage the login of the users and generate the token to cookieparse

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import { User } from "../model/user.js";
+import {  generateToken } from "../utils/generateTokens.js";
 
 
 export const register = async(req,res)=>{
@@ -19,6 +20,28 @@ export const register = async(req,res)=>{
             return res.status(400).json({ error: "this user already exists"})
         } 
         
+        return res.status(500).json({error:" Server error"});
+    }
+};
+
+export const login = async(req,res)=>{
+    const {email,password} = req.body;
+
+    try {
+        let user = await User.findOne({email});
+        if(!user) 
+            return res.status(403).json({error: "user does not exist"})
+
+            const respPassword = await user.comparePassword(password);
+            if(!respPassword)
+                return res.status(403).json({error: "Wrong password"});
+
+        const {token,expiresIn}= generateToken(user.id,res);
+       
+        return res.json({email,token,expiresIn});
+        
+    } catch (error) {
+        console.log(error);
         return res.status(500).json({error:" Server error"});
     }
 };
